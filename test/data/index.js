1fec5866b25ea02018b77912435ed94f1c4c3533
ltfschoen <ltfschoen@gmail.com>
2014-04-07 13:16:53 +1000

    Configured App to Test all End Points with Unit Test. Supplied Helper to Boind App to Sample Data.

---
+++ test/data/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+	"1": {
+		"number": 1,
+		"name": "Littlehumans",
+		"description": "Midwifery app",
+		"stacks": "jQuery",
+		"actualStart": "12:00 PM",
+		"actualFinish": "1:00 PM"
+	},
+	"2": {
+		"number": 2,
+		"name": "Trendmyhunch",
+		"description": "Idea app",
+		"stacks": "Ruby on Rails",
+		"actualStart": "9:00 AM",
+		"actualFinish": "9:30 AM"
+	},
+	"3": {
+		"number": 3,
+		"name": "Lukolio",
+		"description": "Portfolio app",
+		"stacks": "Javascript",
+		"actualStart": "5:00 PM",
+		"actualFinish": "8:30 PM"
+	}
+};
